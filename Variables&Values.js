504d796430f40fdf2ba5f517a3204c6716d174e7
punith kumar ks <punith.ks@sapiens.com>
2021-05-07 16:15:24 +0530

    first javascript basic concept

---
+++ Variables&Values.js
@@ -0,0 +1,28 @@
+//TODO: Variable and values
+
+//Variable ->Variable is a Box which can store a Value or Data.
+//Value -> Value is a piece of Data.
+
+// How to Declare a Variable 
+//the best way of declare a variable is camelCase.
+
+let myName = "punith Kumarr"; //
+let age = 27; 
+let havePassPort = false;
+// here -> let is keyword, myName is a Variable, punith kumarr is a value.
+
+//Valid Variables ->
+// 1) Variable cant be keyword
+// 2) Variable name cant start with Numbers
+// 3) Variable name cant start with special Character Exception $,_(dollor,underscore);
+
+//Right Way of Declaring  Variables
+let myDog = "bunny";
+let $amount = 100;
+let _doYouHaveCreditCard = "yes";
+
+//wrong way of decalring variables
+
+let 1lastName ="ksp"; //Error-> Invalid or unexpected token
+let #result = "pass"; //Error->  Unexpected identifier
+let let = "ksp"; //Error-> let is disallowed as a lexically bound name
