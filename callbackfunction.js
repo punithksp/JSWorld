71164d46db632b685e626f01440677cdb7e4dfa9
punith kumar ks <punith.ks@sapiens.com>
2021-05-11 11:44:55 +0530

    modified callbackfunction

--- callbackfunction.js
+++ callbackfunction.js
@@ -1,41 +1,60 @@
-//TODO: calback function -> you can pass other function into function as arguments is called callback funvction
+//TODO: callback function -> you can pass the other function into function as arguments
 
-let x = function (){
+let x = function(){
     console.log("iam called from inside a function");
 }
-let y = function(cb){
+
+let y =  function(cb){
     console.log("do something");
     cb();
 }
 
-y(x)// o/p -> do something , iam called from inside a function
+y(x);//calling function here i will pass function x as arguments
 
-//try to understand this one then implement next example
-// let  calc = function(operator, num1 , num2){
-//     if(operator === 'add' ){
-//         return num1 +num2;
-//     }
-//     else if(operator === 'mul'){
-//         return num1 * num2;
-//     }
-//     else{
-//     return "not valid operator"
-//     }
-// }
-// let result = calc('add',3,3)
-// console.log(result);
+//normally we are calling  function ->
+let calc = function(operator, num1, num2){
+    if(operator==='add'){
+        return num1 + num2
+    }
+    else if(operator === 'mul'){
+        return num1 * num2
+    }else{
+        return `invalid operator `
+    }
+};
 
-let add  = function(a,b){
-    return a+b
+console.log(calc('add',3,4));7
+console.log(calc('mul',3,5));15
+
+
+//above example we can use callback function ->
+
+let add = function(a,b){
+    return a+b;
 }
 let mul = function(c,d){
     return c*d
 }
-let  calc = function(cb, num1 , num2)
-{
-    return cb(num1,num2);
+let whatEver = function(a,b){
+    console.log(`here  are the value ${a},${b}`)
+}
+let calcs = function(cb,num1,num2){
+  return  cb(num1,num2);
 }
 
-let result = calc(add,2,3);
-let results = calc(mul,2,3);
-console.log(result,  results)// o/p-> 5 6
+console.log(calcs(add,23,3));
+console.log(calcs(mul,5,6));
+console.log(calcs(whatEver,5,6))
+console.log(calcs(function(a,b){return a-b},5,3))//in one line we can do like this also
+
+let number = [1,2,3,45,6,3,4,5];
+
+//more examples
+let check = function(ele)
+{
+    return ele> 2;
+}
+let result = number.filter(check)
+//you write like this also
+let results = number.filter(ele => {return  ele>2})
+console.log(results)
